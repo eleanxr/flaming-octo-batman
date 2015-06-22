cb0ad5f476124b832e562125dd9b7c143865d5ad
Will Dicharry <wdicharry@tnc.org>
2015-06-22 11:10:04 -0600

    Add string methods.

--- flowviz/models.py
+++ flowviz/models.py
@@ -4,13 +4,22 @@ from django.db import models
 class Watershed(models.Model):
     name = models.CharField(max_length = 200)
     
+    def __unicode__(self):
+        return self.name
+    
 class GageLocation(models.Model):
     watershed = models.ForeignKey(Watershed)
     identifier = models.CharField(max_length=200)
     
+    def __unicode__(self):
+        return self.identifier
+    
 class GradedFlowTarget(models.Model):
     location = models.ForeignKey(GageLocation)
     name = models.CharField(max_length=200)
+    
+    def __unicode__(self):
+        return self.name
     
 class GradedFlowTargetElement(models.Model):
     flow_target = models.ForeignKey(GradedFlowTarget)
@@ -19,3 +28,6 @@ class GradedFlowTargetElement(models.Model):
     to_month = models.IntegerField()
     to_day = models.IntegerField()
     target_value = models.DecimalField(max_digits=8, decimal_places=2)
+    
+    def __unicode__(self):
+        return '%s: From %d-%d to %d-%d at %f' % (self.flow_target.name, self.from_month, self.from_day, self.to_month, self.to_day, self.target_value)
